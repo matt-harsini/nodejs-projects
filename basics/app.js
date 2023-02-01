cceff10e00bfbcb0e2842e0c030539adf6805834
Matthew <matthewkimcs@gmail.com>
2023-02-01 13:05:02 -0800

    researched differencies between package.json and package-lock.json

--- basics/app.js
+++ basics/app.js
@@ -12,6 +12,11 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
 
+// package-lock.json
+// some dependencies have dependencies on their own, have those dependencies have versions as well
+// package-lock.json contains all the information/versions about the packages each dependency depends on
+// example: bootstrap requires jquery so that dependency tree/info is stored there
+
 const _ = require("lodash");
 const items = [1, [2, [3, [4]]]];
 const flatItems = _.flattenDeep(items);
