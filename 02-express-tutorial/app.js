b745af24e77fbd6fd36da5c745f5850d139baf6d
Matthew <matthewkimcs@gmail.com>
2023-04-29 15:24:28 -0700

    set up basic server routes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,32 @@
 const http = require("http");
+const { readFileSync } = require("fs");
 
+const homePage = readFileSync("./index.html");
 // callback function runs everytime the server receives an HTTP request
-
 const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.end("<h1>Hello world</h1>");
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    res.write(homePage);
+    res.end();
+    return;
+  }
+  if (url === "/about") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    res.write("<h1>About page</h1>");
+    res.end();
+    return;
+  }
+  res.writeHead(404, {
+    "content-type": "text/html",
+  });
+  res.write("<h1>Resource not found</h1>");
+  res.end();
+  return;
 });
 
 server.listen(5000);
