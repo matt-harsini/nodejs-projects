fd0deecc5592e21e53725ea70d0d3b1679f937da
Matthew <matthewkimcs@gmail.com>
2023-04-29 10:12:00 -0700

    learning about http module

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,10 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+// callback function runs everytime the server receives an HTTP request
+
+const server = http.createServer((req, res) => {
+  console.log("user hit the server");
+  res.end("<h1>Hello world</h1>");
+});
+
+server.listen(5000);
