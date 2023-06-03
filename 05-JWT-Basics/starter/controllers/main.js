c188bc26a7e9d57ef87692e3308b15c50e2af8af
mk975025 <matthewkimcs@gmail.com>
2023-06-02 22:54:11 -0700

    signed jwts

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,11 +1,17 @@
 const APIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const login = async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
     throw new APIError("Please provide email and password", 400);
   }
-  res.send("Fake/Login/Register/Signup Route");
+  const id = new Date().getDate();
+  const token = jwt.sign({ username, id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.send({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
